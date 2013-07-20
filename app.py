9d5a3b91b8c287606e68a764c33a68adca6f0350
Brad Van Vugt <bvanvugt@gmail.com>
2013-07-20 16:50:50 -0700

    Add AI MODES.

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ def _respond(response_json):
     return json.dumps(response_json)
 
 
-@bottle.post('/register')
-def register():
+@bottle.post('/<ai_mode>/register')
+def register(ai_mode):
     request = bottle.request.json
     if not request:
         return "No request data sent"
@@ -24,7 +24,6 @@ def register():
     print "Board:"
     print "  Width:", request.get('board').get('width')
     print "  Height:", request.get('board').get('height')
-    print "  Num Players:", request.get('board').get('num_players')
     print "----------------"
 
     return _respond({
@@ -33,21 +32,22 @@ def register():
     })
 
 
-@bottle.post('/start')
-def start():
+@bottle.post('/<ai_mode>/start')
+def start(ai_mode):
     request = bottle.request.json
     if not request:
         return "No request data sent"
 
     print "--- START ---"
     print "Game ID:", request.get('game_id')
+    print "Num Players:", request.get('num_players')
     print "-------------"
 
     return _respond({})
 
 
-@bottle.post('/tick/<client_id>')
-def tick(client_id):
+@bottle.post('/<ai_mode>/tick/<client_id>')
+def tick(ai_mode, client_id):
     request = bottle.request.json
     if not request:
         return "No request data sent"
@@ -56,6 +56,7 @@ def tick(client_id):
     print "Game ID:", request.get('id')
     print "Turn Num:", request.get('turn_num')
     print "Snakes:", len(request.get('snakes'))
+    # print request.get('board')
     print "----------------"
 
     # Find the last move we made
@@ -63,8 +64,24 @@ def tick(client_id):
         if snake['id'] == client_id:
             my_snake = snake
 
+    random_map = {
+        'n': ['n', 'w', 'e'],
+        's': ['s', 'w', 'e'],
+        'w': ['n', 'w', 's'],
+        'e': ['n', 's', 'e']
+    }
+
+    modes = {
+        'square': ['n', 'n', 'w', 'w', 's', 's', 'e', 'e'],
+        'n': ['n'],
+        's': ['n'],
+        'e': ['e'],
+        'w': ['w'],
+
+        'r': [random.choice(random_map[my_snake['last_move']])]
+    }
     # Map
-    moves = ['n', 'n', 'w', 'w', 's', 's', 'e', 'e']
+    moves = modes[ai_mode][:]
     my_move = moves[request.get('turn_num') % len(moves)]
 
     return _respond({
@@ -73,7 +90,7 @@ def tick(client_id):
     })
 
 
-@bottle.post('/end')
+@bottle.post('<ai_mode>/end')
 def end():
     request = bottle.request.json
     if not request:
